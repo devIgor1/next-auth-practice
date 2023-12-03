32ed069a604018e0ec886ea967bd129cf3778900
Igor <igormraesmm@gmail.com>
2023-12-03 11:58:59 -0300

    build: initial configuration

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react"
+
+interface AppProps {}
+
+const App: FC<AppProps> = ({}) => {
+  return (
+    <div>
+      <h1>Hello APP</h1>
+    </div>
+  )
+}
+
+export default App
