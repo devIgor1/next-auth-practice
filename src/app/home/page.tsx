7fe62f85a7cf0bfcf1263d25735009679a3af002
Igor <igormraesmm@gmail.com>
2023-12-03 13:38:14 -0300

    build: change .env feat: new next-auth config

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,16 @@
+"use client"
+
 import { FC } from "react"
 import { FaArrowDown } from "react-icons/fa6"
+import { signIn } from "next-auth/react"
+import { redirect } from "next/navigation"
 
-const Home: FC = () => {
+const Home: FC = async () => {
+  async function handleLogin() {
+    await signIn()
+    redirect("/dahboard")
+  }
+
   return (
     <div className="w-full min-h-screen flex items-center justify-center flex-col">
       <h1 className="mb-32 text-3xl md:text-5xl">
@@ -14,7 +23,10 @@ const Home: FC = () => {
         <span className="mr-5 -rotate-90">
           <FaArrowDown color="#fff" />
         </span>
-        <button className="border-2 border-white py-2 px-4 rounded font-bold hover:bg-white hover:text-black duration-300 ">
+        <button
+          className="border-2 border-white py-2 px-4 rounded font-bold hover:bg-white hover:text-black duration-300"
+          onClick={handleLogin}
+        >
           Login
         </button>
         <span className="ml-5 rotate-90">
